a2be7da639ba9b37b766bec80d53a72459e39fd6
hoover13 <hoovermtk25@gmail.com>
2025-01-29 14:13:32 -0500

    Initial Commit

---
+++ Astoria Connect/Astoria_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Astoria_ConnectApp.swift
+//  Astoria Connect
+//
+//  Created by Min Thu Khine on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Astoria_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astoria Connect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Astoria Connect
+//
+//  Created by Min Thu Khine on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
